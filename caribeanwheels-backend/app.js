d5d8ef817af488d4ca24d71b1a98e82b049b7b58
mohsin-8 <alikhanmohsin420@gmail.com>
2026-01-02 12:30:23 -0800

    update

--- caribeanwheels-backend/app.js
+++ caribeanwheels-backend/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://caribeanwheelsadmin.vercel.app"],
     credentials: true
 }));
 
