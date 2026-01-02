dfd66076e593c805d6b1784a85bed54531d44915
mohsin-8 <alikhanmohsin420@gmail.com>
2026-01-02 13:44:46 -0800

    update

--- caribeanwheels-backend/controllers/auth.controller.js
+++ caribeanwheels-backend/controllers/auth.controller.js
@@ -49,12 +49,6 @@ exports.login = async (req, res) => {
     process.env.JWT_SECRET,
     { expiresIn: "7d" }
   );
-  // res.cookie("token", token, {
-  //   httpOnly: true,
-  //   secure: process.env.NODE_ENV === "production",
-  //   sameSite: "lax",
-  //   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-  // })
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
--- caribeanwheels-backend/routes/auth.routes.js
+++ caribeanwheels-backend/routes/auth.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { body } = require("express-validator");
 const {
-    register,
-    login,
-    getProfile,
-    forgotPassword,
-    resetPassword
+  register,
+  login,
+  getProfile,
+  forgotPassword,
+  resetPassword
 } = require("../controllers/auth.controller");
 const { protect } = require("../middleware/auth.middleware");
 
@@ -13,25 +13,25 @@ const router = express.Router();
 
 // Register
 router.post(
-    "/register",
-    [
-        body("name").notEmpty().withMessage("Name is required"),
-        body("email").isEmail().withMessage("Valid email required"),
-        body("password")
-            .isLength({ min: 6 })
-            .withMessage("Password must be 6 characters")
-    ],
-    register
+  "/register",
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Valid email required"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be 6 characters")
+  ],
+  register
 );
 
 // Login
 router.post(
-    "/login",
-    [
-        body("email").isEmail().withMessage("Valid email required"),
-        body("password").notEmpty().withMessage("Password required")
-    ],
-    login
+  "/login",
+  [
+    body("email").isEmail().withMessage("Valid email required"),
+    body("password").notEmpty().withMessage("Password required")
+  ],
+  login
 );
 
 router.post(
@@ -57,8 +57,9 @@ router.post("/logout", (req, res) => {
   res
     .clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict"
+      secure: true,
+      sameSite: "none",
+      path: "/",
     })
     .json({ message: "Logged out successfully" });
 });
