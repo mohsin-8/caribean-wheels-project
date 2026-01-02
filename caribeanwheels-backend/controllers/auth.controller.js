90a5365737764927cd8dc3b431486236d87b7b82
mohsin-8 <alikhanmohsin420@gmail.com>
2026-01-02 13:40:24 -0800

    update

--- caribeanwheels-backend/controllers/auth.controller.js
+++ caribeanwheels-backend/controllers/auth.controller.js
@@ -59,7 +59,8 @@ exports.login = async (req, res) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    maxAge: 7 * 24 * 60 * 60 * 1000
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
   })
     .json({
     message: "Login successful",
