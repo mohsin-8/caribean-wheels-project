5c2345d2f96f0c5fda50ff7ee351276f2e9fec3a
mohsin-8 <alikhanmohsin420@gmail.com>
2026-01-02 12:44:24 -0800

    update

--- caribeanwheels-backend/controllers/auth.controller.js
+++ caribeanwheels-backend/controllers/auth.controller.js
@@ -49,20 +49,26 @@ exports.login = async (req, res) => {
     process.env.JWT_SECRET,
     { expiresIn: "7d" }
   );
-  res.cookie("token", token, {
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV === "production",
+  //   sameSite: "lax",
+  //   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+  // })
+  res.cookie("token", jwtToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
   })
     .json({
-      message: "Login successful",
-      user: {
-        name: user.name,
-        email: user.email,
-        role: user.role
-      }
-    });
+    message: "Login successful",
+    user: {
+      name: user.name,
+      email: user.email,
+      role: user.role
+    }
+  });
 };
 
 //forgot Password
