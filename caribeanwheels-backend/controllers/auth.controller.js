b440816c89abfe60de0f86a01af56ad08453466f
mohsin-8 <alikhanmohsin420@gmail.com>
2026-01-02 12:48:34 -0800

    updatte

--- caribeanwheels-backend/controllers/auth.controller.js
+++ caribeanwheels-backend/controllers/auth.controller.js
@@ -55,7 +55,7 @@ exports.login = async (req, res) => {
   //   sameSite: "lax",
   //   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
   // })
-  res.cookie("token", jwtToken, {
+  res.cookie("token", token, {
     httpOnly: true,
     secure: true,
     sameSite: "none",
